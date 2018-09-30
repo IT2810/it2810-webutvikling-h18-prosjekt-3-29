32e0b9c91b5478fa6a7e625a00d2373c43f7726b
ingridlarssen <ingrid_al_96@hotmail.com>
2018-09-30 17:48:20 +0200

    created todo page #5 and changed references to file and component

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,15 +3,15 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
+import TodoScreen from '../screens/TodoScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const TodoStack = createStackNavigator({
+  Home: TodoScreen,
 });
 
-HomeStack.navigationOptions = {
+TodoStack.navigationOptions = {
   tabBarLabel: 'Todo',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
@@ -54,7 +54,7 @@ SettingsStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
+  TodoStack,
   LinksStack,
   SettingsStack,
 });
