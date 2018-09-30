3e3b4a48dd7713370c6ec928e1f572d991d2e2f0
havardhuns <haavarhu@stud.ntnu.no>
2018-09-30 17:02:51 +0200

    endret link tab til step counter #8

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'Step Counter',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
