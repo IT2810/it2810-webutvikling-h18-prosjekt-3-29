8ed8ba7e708bd2659a680a696f6f474d2a8b4088
ingridlarssen <ingrid_al_96@hotmail.com>
2018-10-12 13:14:03 +0200

    validation-box when finishing todo #31

--- components/TodoElement.js
+++ components/TodoElement.js
@@ -6,34 +6,57 @@ import {
   View,
 } from 'react-native';
 import { CheckBox, ListItem, Body } from 'native-base';
+import Dialog from "react-native-dialog";
 
 export default class TodoElement extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: this.props.text,
-      checked: false
+      checked: false,
+      dialogVisible: false
     }
   }
 
   checkBoxChecked = () => {
     this.props.removeTodoElement(this.props.text);
+    this.setState({ dialogVisible: false });
   }
+
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ dialogVisible: false });
+  };
 
   render() {
     return (
       <View>
-        <ListItem>
-          <CheckBox style={styles.checkBox}
-            center
-            title='Todo'
-            checked={this.state.checked}
-            onPress={this.checkBoxChecked}
-          />
-          <Body>
-            <Text  style={styles.checkBoxText}>{this.props.text}</Text>
-          </Body>
-        </ListItem>
+        <View>
+          <ListItem>
+            <CheckBox style={styles.checkBox}
+              center
+              title='Todo'
+              checked={this.state.checked}
+              onPress={this.showDialog}
+            />
+            <Body>
+              <Text  style={styles.checkBoxText}>{this.props.text}</Text>
+            </Body>
+          </ListItem>
+        </View>
+        <View>
+          <Dialog.Container visible={this.state.dialogVisible}>
+            <Dialog.Title>Finish todo</Dialog.Title>
+            <Dialog.Description>
+            Do you want to finish this todo?
+            </Dialog.Description>
+            <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+            <Dialog.Button label="Finish" onPress={this.checkBoxChecked} />
+          </Dialog.Container>
+        </View>
       </View>
     );
   }
